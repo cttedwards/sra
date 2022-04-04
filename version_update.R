9653f2c960ffdcdab096867f3b86c80d458e847a
Charles <cescapecs@gmail.com>
2022-04-04 17:17:30 +0200

    Create version_update.R

---
+++ version_update.R
@@ -0,0 +1,19 @@
+# Get Version and date
+VERSION <- scan('DESCRIPTION',what = character(),skip = 3,nlines = 1)[2]
+DATE    <- Sys.Date()
+TIME    <- Sys.time()
+
+# update DESCRIPTION
+DESCRIPTION    <- readLines('DESCRIPTION')
+DESCRIPTION[4] <- paste('Version:', VERSION)
+DESCRIPTION[5] <- paste('Date:', DATE)
+writeLines(DESCRIPTION, 'DESCRIPTION')
+rm(DESCRIPTION)
+
+# Write .onAttach
+filename <- "R/zzz.R"
+cat(".onAttach <- function(libname, pkgname)\n", file = filename)
+cat("{\n", file = filename, append = TRUE)
+cat(paste("    packageStartupMessage(\"sra v.", VERSION, " (", TIME, ");\\nInputs compiled using: sraInputsBio v.", packageVersion("sraInputsBio"), "; sraInputs v.", packageVersion("sraInputs"), "\")\n", sep = ""), file = filename, append = TRUE)
+cat("}\n", file = filename, append = TRUE)
+rm(filename)
