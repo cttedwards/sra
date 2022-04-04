843a4666abbaa79ea363dcf34e0793b4162c052c
Charles <cescapecs@gmail.com>
2022-04-04 17:17:47 +0200

    tidy up

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname)
 {
-    packageStartupMessage("sra version 0.0.3 (2021-12-06)")
+    packageStartupMessage("sra v.0.0.3 (2022-04-04 17:01:46);\nInputs compiled using: sraInputsBio v.2.0.0; sraInputs v.0.2.0")
 }
