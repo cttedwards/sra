2589e0d695f4dc5846955f4e4206fc3ac6f079fa
Charles <cescapecs@gmail.com>
2022-06-01 23:34:08 +0200

    Update version_update.R

--- version_update.R
+++ version_update.R
@@ -1,5 +1,14 @@
+versionUpdate <- function(version, update = "0.0.0") {
+  
+  version <- as.integer(unlist(strsplit(version, split = ".", fixed = TRUE)))
+  update  <- as.integer(unlist(strsplit(update,  split = ".", fixed = TRUE)))
+  
+  return(paste(version + update, collapse = "."))
+}
+
 # Get Version and date
 VERSION <- scan('DESCRIPTION',what = character(),skip = 3,nlines = 1)[2]
+VERSION <- versionUpdate(VERSION, "0.0.1")
 DATE    <- Sys.Date()
 TIME    <- Sys.time()
 
@@ -14,6 +23,6 @@ rm(DESCRIPTION)
 filename <- "R/zzz.R"
 cat(".onAttach <- function(libname, pkgname)\n", file = filename)
 cat("{\n", file = filename, append = TRUE)
-cat(paste("    packageStartupMessage(\"sra v.", VERSION, " (", TIME, ");\\nInputs compiled using: sraInputsBio v.", packageVersion("sraInputsBio"), "; sraInputs v.", packageVersion("sraInputs"), "\")\n", sep = ""), file = filename, append = TRUE)
+cat(paste("    packageStartupMessage(\"sra v.", substr(VERSION, start = 1, stop = 3), " (", TIME, ");\\nInputs compiled using: sraInputsBio v.", packageVersion("sraInputsBio"), "; sraInputs v.", packageVersion("sraInputs"), "\")\n", sep = ""), file = filename, append = TRUE)
 cat("}\n", file = filename, append = TRUE)
 rm(filename)
