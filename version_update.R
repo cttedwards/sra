966af0b64c99b3581de2ba3e341861c50080cd6a
Charles <cescapecs@gmail.com>
2022-11-07 20:56:51 +0200

    Update version_update.R

--- version_update.R
+++ version_update.R
@@ -23,6 +23,6 @@ rm(DESCRIPTION)
 filename <- "R/zzz.R"
 cat(".onAttach <- function(libname, pkgname)\n", file = filename)
 cat("{\n", file = filename, append = TRUE)
-cat(paste("    packageStartupMessage(\"sra v.", substr(VERSION, start = 1, stop = 3), " (", TIME, ");\\nInputs compiled using: sraInputsBio v.", packageVersion("sraInputsBio"), "; sraInputs v.", packageVersion("sraInputs"), "\")\n", sep = ""), file = filename, append = TRUE)
+cat(paste("    packageStartupMessage(\"sra v.", VERSION, " (", TIME, ");\\nInputs compiled using: sraInputsBio v.", packageVersion("sraInputsBio"), "; sraInputs v.", packageVersion("sraInputs"), "\")\n", sep = ""), file = filename, append = TRUE)
 cat("}\n", file = filename, append = TRUE)
 rm(filename)
