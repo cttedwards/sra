d57baa38facccb3bfee6390887ff2f3af3f8f2a7
Charles <cescapecs@gmail.com>
2022-11-07 20:57:03 +0200

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname)
 {
-    packageStartupMessage("sra v.0.1 (2022-10-29 20:03:36);\nInputs compiled using: sraInputsBio v.2.0.0; sraInputs v.0.2.2")
+    packageStartupMessage("sra v.0.2 (2022-11-07 08:33:28);\nInputs compiled using: sraInputsBio v.2.0.0; sraInputs v.1.0.2")
 }
