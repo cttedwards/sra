53bd0c2b9fdce675d8fb8567655296b20c2a2e3e
Charles <cescapecs@gmail.com>
2022-11-08 15:47:38 +0200

    rebuild v0.2.4
    with final versions of sraInputsBio_2.0.0 and sraInputs_1.0.2

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname)
 {
-    packageStartupMessage("sra v.0.2 (2022-11-07 08:33:28);\nInputs compiled using: sraInputsBio v.2.0.0; sraInputs v.1.0.2")
+    packageStartupMessage("sra v.0.2.4 (2022-11-08 15:41:42);\nInputs compiled using: sraInputsBio v.2.0.0; sraInputs v.1.0.2")
 }
